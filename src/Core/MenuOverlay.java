18b169e5d9a4d6b537e21835bec211b621f61dcf
WolfgangBischoff <wolfgang.bischoff@outlook.at>
2020-04-23 23:38:40 +0200

    improved Inventory GUI

--- src/Core/MenuOverlay.java
+++ src/Core/MenuOverlay.java
@@ -29,15 +29,32 @@ public class MenuOverlay
     {
         String methodName = "draw() ";
         menuGc.clearRect(0, 0, MENU_WIDTH, MENU_HEIGHT);
+        Color background = Color.rgb(60, 90, 85);
+        double hue = background.getHue();
+        double sat = background.getSaturation();
+        double brig = background.getBrightness();
+        Color marking = Color.hsb(hue, sat - 0.2, brig + 0.2);
+        Color font = Color.hsb(hue, sat + 0.15, brig + 0.4);
 
         menuGc.setGlobalAlpha(0.8);
-        menuGc.setFill(Color.DARKSLATEGREY);
+        menuGc.setFill(background);
         menuGc.fillRect(0, 0, MENU_WIDTH, MENU_HEIGHT);
 
         menuGc.setGlobalAlpha(1);
-        menuGc.setFill(Color.BLANCHEDALMOND);
-        menuGc.fillText("Game Menu " + player.actorInGameName, 30, 30);
+        menuGc.setFill(marking);
 
+        for (int y = 0; y < 3; y++)
+        {
+            int offsetY = y * (64 + 10) + 40;
+            for (int i = 0; i < 6; i++)
+            {
+                int squareX = i * (64 + 10) + 10;
+                menuGc.fillRect(squareX, offsetY, 64, 64);
+            }
+        }
+
+        menuGc.setFill(font);
+        menuGc.fillText("Game Menu " + player.actorInGameName, 30, 30);
         menuGc.fillText(player.inventory.itemsList.toString(), 30, 60);
 
         SnapshotParameters transparency = new SnapshotParameters();
